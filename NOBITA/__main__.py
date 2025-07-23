f5266eb006fb8764eccb1ee1719a8a4434149244
HKMUSIC <himanshu4hk9@gmail.com>
2025-07-23 13:23:53 +0530

    Update __main__.py

--- NOBITA/__main__.py
+++ NOBITA/__main__.py
@@ -47,7 +47,8 @@ async def init():
     await safe.start(NOBITA)
     try:
         await NOBITA.stream_call("https://te.legra.ph/file/...")
-except asyncio.TimeoutError:
+    except asyncio.TimeoutError:
+    print("Telegram request timed out")
     LOGGER("NOBITA").error("❌ Timeout while trying to stream.")
             "𝗣𝗹𝗭 𝗦𝗧𝗔𝗥𝗧 𝗬𝗢𝗨𝗥 𝗟𝗢𝗚 𝗚𝗥𝗢𝗨𝗣 𝗩𝗢𝗜𝗖𝗘𝗖𝗛𝗔𝗧\𝗖𝗛𝗔𝗡𝗡𝗘𝗟\n\n𝗡𝗢𝗕𝗜𝗧𝗔 𝗕𝗢𝗧 𝗦𝗧𝗢𝗣........"
         )
