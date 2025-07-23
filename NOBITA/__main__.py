eff4d461d0b9d51ed8218e7fc0e8e12868d3bc3d
HKMUSIC <himanshu4hk9@gmail.com>
2025-07-23 12:13:28 +0530

    Update __main__.py

--- NOBITA/__main__.py
+++ NOBITA/__main__.py
@@ -46,9 +46,9 @@ async def init():
     await safe.start(userbot)
     await safe.start(NOBITA)
     try:
-        await NOBITA.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
-    except NoActiveGroupCall:
-        LOGGER("NOBITA").error(
+    await NOBITA.stream_call("https://te.legra.ph/file/...")
+except asyncio.TimeoutError:
+    LOGGER("NOBITA").error("❌ Timeout while trying to stream.")
             "𝗣𝗹𝗭 𝗦𝗧𝗔𝗥𝗧 𝗬𝗢𝗨𝗥 𝗟𝗢𝗚 𝗚𝗥𝗢𝗨𝗣 𝗩𝗢𝗜𝗖𝗘𝗖𝗛𝗔𝗧\𝗖𝗛𝗔𝗡𝗡𝗘𝗟\n\n𝗡𝗢𝗕𝗜𝗧𝗔 𝗕𝗢𝗧 𝗦𝗧𝗢𝗣........"
         )
         exit()
